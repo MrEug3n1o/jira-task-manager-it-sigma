349bb4a181623bb703605df1dccaf444cbe5aaa2
mr_eugene <roberttimethone@gmail.com>
2025-10-31 21:36:06 +0100

    Add models to admin panel

--- smart_task_manager/settings.py
+++ smart_task_manager/settings.py
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
+AUTH_USER_MODEL = "task_manager.Worker"
 
 LANGUAGE_CODE = 'en-us'
 
--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Worker,
+                     Task,
+                     TaskType,
+                     Position,
+                     )
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "position")
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "priority", "task_type")
+    filter_horizontal = ("assignees",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
