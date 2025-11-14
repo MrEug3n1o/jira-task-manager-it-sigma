7fbcd0552f04c87c13caaae936435f3118ca70e9
mr_eugene <roberttimethone@gmail.com>
2025-11-14 22:16:25 +0100

    static uncommented

--- smart_task_manager/settings.py
+++ smart_task_manager/settings.py
@@ -130,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 #
 # # This production code might break development mode, so we check whether we're in DEBUG mode
 # if not DEBUG:
